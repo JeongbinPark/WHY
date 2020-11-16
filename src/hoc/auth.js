3e74a80a8e360e41061b635d79282175145c2248
Jeongbinpark <qkrwjdqls99@gmail.com>
2020-11-17 00:24:07 +0900

    configuration to connect with apollo should compare with JH's configurations, and merge

--- src/hoc/auth.js
+++ src/hoc/auth.js
@@ -4,6 +4,22 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { authUser } from "../_actions/userAction";
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+/*
+const GET_VERTIFICATION = gql `
+  query {
+    Verification {
+      id
+      target
+      payload
+      key
+      verified
+  }
+`*
+*/
+
 
 export default (SpecificComponent, option, adminRoute = null) => {
   // option 값
@@ -12,6 +28,37 @@ export default (SpecificComponent, option, adminRoute = null) => {
   // false    => 로그인한 유저는 출입 불가능한 페이지
   // admin은 true주면됨
   const AuthenticationCheck = (props) => {
+/* GET_VERTIFICATION 관한 데이터 없어서 전체 주석처리 
+    이후에 편집 필요
+    const { loading, error, data } = useQuery(GET_VERTIFICATION);
+    useEffect(()=>{
+      console.log(data.Verification.verified);
+
+        // 이메일 인증 안된 사용자는 홈으로 팅군다.
+        if (option && !data.Verification.verified) {
+          alert("이메일 인증을 진행해주세요.");
+          props.history.push("/");
+        }
+        /////////////
+        // isn't login
+        if (!response.payload.isAuth) {
+          if (option) {
+            props.history.push("/login");
+          }
+        } else {
+          // is login
+          if (adminRoute && !response.payload.isAdmin) {
+            props.history.push("/");
+          } else {
+            if (option === false) {
+              props.history.push("/");
+            }
+          }
+        }
+        ///////////////
+    })
+    */
+    /*
     const dispatch = useDispatch();
     useEffect(() => {
       // dispatch
@@ -40,7 +87,7 @@ export default (SpecificComponent, option, adminRoute = null) => {
           }
         }
       });
-    }, []);
+    }, []);*/
     return <SpecificComponent />;
   };
   return AuthenticationCheck;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,83 @@ import { applyMiddleware, createStore } from "redux";
 import promiseMiddleware from "redux-promise";
 import ReduxThunk from "redux-thunk";
 import Reducer from "./_reducers";
+//import ApolloClient ,{ InMemoryCache} from 'apollo-boost';
+import { InMemoryCache } from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
 
+import { createHttpLink, HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloLink, ApolloProvider } from '@apollo/react-hooks'; //react-apollo 이용하면 에러
+//import { InMemoryCache } from 'apollo-cache-inmemory';
+
+
+const httpLink = new HttpLink({ 
+  uri : 'http://localhost:4000',
+  credentials : 'include'
+});
+/*
+const authLink = new ApolloLink((operation, forward) => {
+  //Retrive the authorization token from local storage
+  const token = localStorage.getItem('auth_token');
+
+  //use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      //authorization: "ZGFpc3lAYXBvbGxvZ3JhcGhxbC5jb20=" 
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  });
+
+  //call the next link in the middleware chain
+  return forward(operation);
+});
+*/
+
+const client = new ApolloClient({
+  link : new HttpLink({
+    headers:{authorization:  "ZGFpc3lAYXBvbGxvZ3JhcGhxbC5jb20="},
+     //localStorage.getItem('token')},
+    uri : "http://localhost:4000",
+  }),
+  //uri: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+/*
+const client = new ApolloClient({
+  uri : 'http://localhost:4000',
+  cache: new InMemoryCache()
+})
+*/
+/*
+const httpLink = createHttpLink ({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const authLink = setContext((_, {headers}) =>{
+  const token = localStorage.getItem('token');
+  return {
+    headers:{
+      ...headers,
+      //authorization : token ? `Bearer ${token}` : "",
+      authorization: token
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+*/
+{/*앱 내 모든 컴포넌트에서 GraphQL API 연동 가능 */}
+ReactDOM.render(
+  <ApolloProvider client={client}>  
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+)
+
+/*
 const createStoreWithMiddleware = applyMiddleware(
   promiseMiddleware,
   ReduxThunk
@@ -27,7 +103,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
+*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
